7e5adac32c9eb527f97437bf737b625e0d982b3f
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-06 20:58:23 +0200

    initial commit

--- install_lib_bash_wine.sh
+++ install_lib_bash_wine.sh
@@ -14,8 +14,8 @@ function update_lib_bash_wine_if_exist {
         cd lib_bash_wine
         local git_remote_hash=$(git --no-pager ls-remote --quiet | grep HEAD | awk '{print $1;}' )
         local git_local_hash=$(git --no-pager log --decorate=short --pretty=oneline -n1 | grep HEAD | awk '{print $1;}' )
-        git fetch --all
-        git reset --hard origin/master
+        git fetch --all  > /dev/null 2>&1
+        git reset --hard origin/master  > /dev/null 2>&1
         chmod -R +x ./*.sh
         cd ..
     fi
